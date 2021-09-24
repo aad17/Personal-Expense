ffa272dbd1a1292880f89e65762f3ef8dcc9f12c
Amey <ameydeshingkar@gmail.com>
2021-09-24 20:19:08 +0530

    Some Styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,10 +45,32 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: [
                       Container(
-                        child: Text(tx.amount.toString()),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text("\$${tx.amount}",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            )),
                       ),
                       Column(
-                        children: [Text(tx.title), Text(tx.date.toString())],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.purple),
+                          ),
+                          Text(DateFormat.yMMMMEEEEd().format(tx.date),
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 10))
+                        ],
                       )
                     ],
                   ),
